80ee766ac727e414ce1f4a7a022f4664854ef558
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-02-16 15:58:49 -0300

    Woking command line parsing

--- trabalho/TRABALHO.asm
+++ trabalho/TRABALHO.asm
@@ -32,8 +32,8 @@ line_0 db 'Linha $'
 line_1 db ' Valor inválido: $'
 
 ;Variables
+cmd_size db 0 ;Size of command line
 cmd db 128 dup(0) ;Buffer for command line
-cmd_size dw 0 ;Size of command line
 infile db 64 dup(0) ;Buffer for input file
 outfiles db 64 dup(0) ;Buffer for output file
 voltage db 64 dup(0) ;Buffer for voltage
@@ -42,46 +42,92 @@ voltage db 64 dup(0) ;Buffer for voltage
 .code
 .startup
 
+;PSP argument collection. Provided by the assignment
+push ds ; Salva as informações de segmentos
+push es
+
+mov ax,ds; Troca DS com ES para poder usa o REP MOVSB
+mov bx,es
+mov ds,bx
+mov es, ax
+mov si,80h; Obtém o tamanho do string da linha de comando e coloca em CX
+mov ch,0
+mov cl,[si]
+mov ax,cx ; Salva o tamanho do string em AX, para uso futuro
+mov si,81h; Inicializa o ponteiro de origem
+lea di,cmd ; Inicializa o ponteiro de destino
+rep movsb
+
+pop es ; retorna as informações dos registradores de segmentos
+pop ds
+
+mov cmd_size, al ; Store command size
+
+;;Check argument size
+cmp cmd_size, 0
+je e_noparami
+
+;Print commadn line
+lea bx, cmd
+call printf_s
 
 .exit 0 ; Return to OS (err code 0)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Errors
 e_noparami:
 lea dx, noparam_i
-call printstring
+call printf_dos
 .exit 1
 
 e_noparamo:
 lea dx, noparam_o
-call printstring
+call printf_dos
 .exit 2
 
 e_noparamv:
 lea dx, noparam_v
-call printstring
+call printf_dos
 .exit 3
 
 e_wrongparamv:
 lea dx, wrongparam_v
-call printstring
+call printf_dos
 .exit 4
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;Functions
 
-;Input: AL = character to print
+;Print char (Native to MSDOS)
+;Input: DL = character to print
 ;Output: None
-printchar proc near
+putchar proc near
     mov ah,02h
     int 21h
     ret
-printchar endp
+putchar endp
 
+;Print $ terminated string (Native to MSDOS)
 ;Input: DS:DX = pointer to string to print (terminated with $)
 ;Output: None
-printstring proc near
+printf_dos proc near
     mov ah,09h
     int 21h
     ret
-printstring endp
+printf_dos endp
+
+;Print \0 terminated string
+;Input: BX = pointer to string to print (terminated with \0)
+;Output: None
+printf_s	proc	near
+	mov		dl,[bx]
+	cmp		dl,0
+	je		ps_1
+	push	bx
+    call    putchar
+	pop		bx
+	inc		bx
+    jmp		printf_s		
+    ps_1:
+	ret
+printf_s	endp
 
 END
